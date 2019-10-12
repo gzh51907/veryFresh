768d2b2fa0758f4ea640ca1728dbd6c233b13720
lh <1013234170@qq.com>
2019-10-12 17:10:55 +0800

    项目准备

---
+++ jxw/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>index</h2>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style>
---
+++ jxw/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+//引入element-ui 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 引入路由配置的参数,并注入Vue 实例
+import router from './route'
+
+//把 axios 写入Vue的原型，成为Vue的属性 在任何地方都可以使用了
+import axios from 'axios';
+Vue.prototype.$axios = axios;
+
+new Vue({
+  router,   //注入实例
+  render: h => h(App),
+}).$mount('#app')
