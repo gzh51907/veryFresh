f83d1b29bd5a0ee70d6a2db6710ae238e6dedbff
luoxiaofang <974721788@qq.com>
2019-10-14 09:40:32 +0800

    完成了分类的页面

--- jxw/src/main.js
+++ jxw/src/main.js
@@ -8,6 +8,10 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+
 import './css/base.css';
 import './js/rem.js'
 
